b2f7ea61c0201a8bf96116004fed64730015f402
zhaokewei <zhaokewei1992@gmail.com>
2019-05-16 00:28:11 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="qrcode-block">
+            <img class="qrcode" src="../assets/qrcode-wx.jpg" >
+            <img class="qrcode" src="../assets/qrcode_zfb.jpg" >
+        </div>
+        <div class="begimg-block">
+            <img class="begimg" src="../assets/beg.jpg">
+        </div>
+        <div>美女帅哥，行行好，给点钱吧！</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+    .qrcode-block {
+        box-shadow: cadetblue 10px 10px 10px 10px;
+        width: 60%;
+        margin: auto;
+    }
+    .qrcode {
+        width: 30%;
+        margin: 5%;
+    }
+    .begimg-block{
+        margin-top: 4%;
+    }
+    .begimg {
+        width: 20%;
+        height: 20%;
+    }
+</style>
